acd777c8ac3757cac22447fe9777221ef997575e
aidoskz3 <aidos.sib@mail.ru>
2022-09-06 18:32:08 +0600

    build(app): initialize the project

---
+++ project-base-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project-base-ios
+//
+//  Created by Aidos Serikbayev on 06.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
